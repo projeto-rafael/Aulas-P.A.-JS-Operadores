a422dda2dcd6477a0763ab8f9e2f00077e2bf695
projeto-rafael <80163684+projeto-rafael@users.noreply.github.com>
2021-11-28 10:21:32 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,38 @@
+var num1 = parseInt(prompt('digite o primeiro numero'));
+var num2 = parseInt(prompt('digite o segundo numero'));
+
+var soma = num1 + num2;
+
+var subtracao = num1 - num2;
+
+var multiplicacao = num1*num2;
+
+var divisao = num1/num2;
+
+//primeiro numero
+document.getElementById('num1').innerHTML = num1;
+//segundo numero
+document.getElementById('num2').innerHTML = num2;
+//soma
+document.getElementById('soma').innerHTML = soma;
+
+//primeiro numero
+document.getElementById('num1').innerHTML = num1;
+//segundo numero
+document.getElementById('num2').innerHTML = num2;
+//subtraçao
+document.getElementById('subtracao').innerHTML = subtracao;
+
+//primeiro numero
+document.getElementById('num1').innerHTML = num1;
+//segundo numero
+document.getElementById('num2').innerHTML = num2;
+//multiplicaçao
+document.getElementById('multiplicacao').innerHTML = multiplicacao;
+
+//primeiro numero
+document.getElementById('num1').innerHTML = num1;
+//segundo numero
+document.getElementById('num2').innerHTML = num2;
+//soma
+document.getElementById('divisao').innerHTML = divisao;
